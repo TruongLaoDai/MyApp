b8a42c1f0cf18fd2de6aa5108713a9e23c4bde6a
TruongLaoDai <tp890981@gmail.com>
2023-10-08 22:02:31 +0700

    Cải tiến hiệu năng lần 1

--- app/src/main/java/com/smile/watchmovie/activity/SettingPlayFilmActivity.java
+++ app/src/main/java/com/smile/watchmovie/activity/SettingPlayFilmActivity.java
@@ -6,23 +6,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.smile.watchmovie.R;
 import com.smile.watchmovie.databinding.ActivitySettingPlayFilmBinding;
 
 public class SettingPlayFilmActivity extends AppCompatActivity {
-
     private ActivitySettingPlayFilmBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting_play_film);
+        binding = ActivitySettingPlayFilmBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        binding = ActivitySettingPlayFilmBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
         boolean full_screen = sharedPreferences.getBoolean("full_screen", false);
         boolean auto_play = sharedPreferences.getBoolean("auto_play", true);
@@ -30,26 +26,12 @@ public class SettingPlayFilmActivity extends AppCompatActivity {
         binding.swSettingPlayFullScreen.setChecked(full_screen);
         binding.swSettingAutoPlay.setChecked(auto_play);
 
-        binding.titleAutoFullScreen.setOnClickListener(v -> {
-            binding.swSettingPlayFullScreen.setChecked(!binding.swSettingPlayFullScreen.isChecked());
-            editor.putBoolean("full_screen", binding.swSettingPlayFullScreen.isChecked());
-            editor.apply();
-        });
-
-        binding.titleAutoPlay.setOnClickListener(v -> {
-            binding.swSettingAutoPlay.setChecked(!binding.swSettingAutoPlay.isChecked());
-            editor.putBoolean("auto_play", binding.swSettingAutoPlay.isChecked());
-            editor.apply();
-        });
-
         binding.swSettingPlayFullScreen.setOnClickListener(v -> {
-            binding.swSettingPlayFullScreen.setChecked(binding.swSettingPlayFullScreen.isChecked());
             editor.putBoolean("full_screen", binding.swSettingPlayFullScreen.isChecked());
             editor.apply();
         });
 
         binding.swSettingAutoPlay.setOnClickListener(v -> {
-            binding.swSettingAutoPlay.setChecked(binding.swSettingAutoPlay.isChecked());
             editor.putBoolean("auto_play", binding.swSettingAutoPlay.isChecked());
             editor.apply();
         });
